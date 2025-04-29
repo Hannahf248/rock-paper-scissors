8bd5e4d09814e6dda2d03bb0ee8586974282693a
Hannah Ford <131198359+Hannahf248@users.noreply.github.com>
2025-04-29 20:58:08 +0100

    write getHumanChoice function

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // create function getComputerChoice which returns a variable (rock, paper scissors)
-
 function getComputerChoice() {
-    let choice = Math.random() * 3;
-    if (choice <= 1) {
-        choice = "rock";
-    } else if (choice <= 2) {
-        choice = "paper";
+    // use Math.random (returns equal to 0 up to 1)
+    let computerChoice = Math.random() * 3;
+    if (computerChoice <= 1) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 2) {
+        computerChoice = "paper";
     } else {
-        choice = "scissors";
+        computerChoice = "scissors";
     }
-    return choice;
+   // function returns variable (rock, paper scissors)
+    return computerChoice;
+}
+
+// create function getHumanChoice (use prompt method to get user input)
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter Rock, Paper or Scissors")
+// return the users choice
+return humanChoice;
+
+
 }
 
 
-// use Math.random (returns equal to 0 up to 1)
 
-// function returns variable (rock, paper scissors)
+

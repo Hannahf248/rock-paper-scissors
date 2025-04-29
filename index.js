87b203a8a897787e07834054493d05bfc57e5d28
Hannah Ford <131198359+Hannahf248@users.noreply.github.com>
2025-04-29 21:24:30 +0100

    start function playRound

--- index.js
+++ index.js
@@ -26,6 +26,38 @@ return humanChoice;
 let humanScore = 0;
 let computerScore = 0;
 
+// create logic to play a single round
+// create new function playRound
 
+function playRound(humanChoice, computerChoice) {
+// make human choice case insensitive using .toLowerCase
+
+
+// if computer and human choose same variable = draw
+
+// if computer chooses rock and human chooses scissors = human loses
+
+// if computer chooses scissors and human chooses paper = human loses
+
+// if computer chooses paper and human chooses rock = human loses
+
+// console log human loses
+
+// if computer chooses scissors and human chooses rock = human wins
+
+// if computer chooses paper and human chooses scissors = human wins
+
+// if computer chooses rock and human chooses paper = human wins
+
+// console log human wins
+
+// increment human score or computer score variable based on round winner
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+}
 
 

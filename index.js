9d4f13758051563ac30bf8565dabd354a330dd12
Hannah Ford <131198359+Hannahf248@users.noreply.github.com>
2025-04-30 19:11:12 +0100

    enter human loses condition

--- index.js
+++ index.js
@@ -43,11 +43,12 @@ if (humanChoice === computerChoice) {
 // if computer chooses rock and human chooses scissors = human loses
 // if computer chooses scissors and human chooses paper = human loses
 // if computer chooses paper and human chooses rock = human loses
- else if (computerChoice === "rock" && humanChoice === "scissors" || 
-        computerChoice === "scissors" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "rock") {
-    computerChoice = computerChoice ++;
-    return "You lose!"
- }
+const humanLoses = (
+(computerChoice === "rock" && humanChoice === "scissors") || 
+        (computerChoice === "scissors" && humanChoice === "paper") || 
+        (computerChoice === "paper" && humanChoice === "rock") 
+ 
+        )
 }
 
 // console log human loses
